657c8119f28f5a7db6ab04a6e28240120ed9c406
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-09-24 20:11:51 -0300

    atualização 24-09
    Estrutura aninhada

---
+++ Exercícios Python/Aula etrutura aninhada.py
@@ -0,0 +1,10 @@
+nome = str(input('Qual é eu nome? '))
+if nome == 'William':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil!')
+elif nome in 'Ana Cláudia Jéssica Juliana':
+    print('Que belo nome feminino!')
+else:
+    print('Seu nome é nem normal') 
+print('Tenha um bom dia, {}.'.format(nome))
