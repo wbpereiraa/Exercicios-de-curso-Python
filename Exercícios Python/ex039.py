7b392855ee5ca36cc82b5840025eb6995f004536
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-09-29 12:53:12 -0300

    atualização 29-09

--- Exercícios Python/ex039.py
+++ Exercícios Python/ex039.py
@@ -1,19 +1,22 @@
-from ast import Break
 from datetime import date
 atual = date.today().year
+sexo = 'F'
+sexo = (input('Qual seu sexo? [F] ou [M]? ')).upper()
 ano = int(input('Digite o ano de nascimento: '))
 idade = atual - ano
 print('Quem nasceu em {} tem {} anos em {}.'.format(ano, idade, atual))
-if idade == 18:
-    print('Você precisa se alistar IMEDIATAMENTE!')
-elif idade > 18:
-    condição = idade - 18
-    condição1 = atual - condição
-    print('Seu alistamento está atrasado há {} ano(s) e seu alistamento foi em {}.'.format(condição, condição1))
-elif idade < 18:
-    condição2 = 18 - idade
-    condição3 = atual + condição2
-    print('Ainda falta {} ano(s) para seu alistamento. Seu alistamento será em {}.'.format(condição2, condição3))
-
-      
+while sexo =='F':
+    print('Alistamento não obrigatório!')
+    break
+else:
+    if idade == 18:
+        print('Você precisa se alistar IMEDIATAMENTE!')
+    if idade > 18:
+        condição = idade - 18
+        condição1 = atual - condição
+        print('Seu alistamento está atrasado há {} ano(s) e seu alistamento foi em {}.'.format(condição, condição1))
+    if idade < 18:
+        condição2 = 18 - idade
+        condição3 = atual + condição2
+        print('Ainda falta {} ano(s) para seu alistamento. Seu alistamento será em {}.'.format(condição2, condição3))
     
