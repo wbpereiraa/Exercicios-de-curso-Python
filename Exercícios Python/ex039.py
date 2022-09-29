955416014534a6ed6b560cc4867990d91f334f94
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-09-29 12:59:15 -0300

    programa finalizado

--- Exercícios Python/ex039.py
+++ Exercícios Python/ex039.py
@@ -2,13 +2,13 @@ from datetime import date
 atual = date.today().year
 sexo = 'F'
 sexo = (input('Qual seu sexo? [F] ou [M]? ')).upper()
-ano = int(input('Digite o ano de nascimento: '))
-idade = atual - ano
-print('Quem nasceu em {} tem {} anos em {}.'.format(ano, idade, atual))
 while sexo =='F':
     print('Alistamento não obrigatório!')
     break
 else:
+    ano = int(input('Digite o ano de nascimento: '))
+    idade = atual - ano
+    print('Quem nasceu em {} tem {} anos em {}.'.format(ano, idade, atual))
     if idade == 18:
         print('Você precisa se alistar IMEDIATAMENTE!')
     if idade > 18:
