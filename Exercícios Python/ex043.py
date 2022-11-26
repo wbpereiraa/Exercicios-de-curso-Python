1729f4de7e298ee4abe630bed42862ed39269d1e
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-11-26 11:16:54 -0300

    Create ex043.py

---
+++ Exercícios Python/ex043.py
@@ -0,0 +1,16 @@
+peso = float(input('Qual o seu peso? (KG)'))
+altura = float(input('Qual a sua altura? '))
+imc = peso / (altura**2)
+print('O IMC desta pessoa é {:.2f}.'.format(imc))
+if imc < 18.5:
+    print('ATENÇÃO! Você esta abaixo do Peso!')
+if imc > 18.5 and imc <= 25:
+    print('Você esta no Peso ideal!')
+if imc > 25 and imc <=30:
+    print('Você esta com Sobrepeso!')
+if imc > 30 and imc <= 40:
+    print('Você esta com Obesidade!')
+elif imc > 40:
+    print('ATENÇÃO! Você esta com Obesidade Mórbida!')
+    
+        
