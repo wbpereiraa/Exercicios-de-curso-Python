52bf9502ac267ed1209531d86b6c95aed4b5200e
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-12-04 08:52:03 -0300

    Update ex050.py

--- Exercícios Python/ex050.py
+++ Exercícios Python/ex050.py
@@ -4,5 +4,5 @@ for num in range(1, 7):
     valor = int(input('Digite o {}º valor: '.format(num)))
     if valor % 2 == 0:
         soma += valor
-print('A soma dos números inteiro pares é igual a {}.'.format(soma))
+print('A soma dos números inteiros pares é igual a {}.'.format(soma))
 
