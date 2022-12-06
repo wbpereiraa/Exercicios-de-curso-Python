c4622c0ec07f219f08919532818beb6e44459c9e
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-12-06 08:48:40 -0300

    Update ex054.py

--- Exercícios Python/ex054.py
+++ Exercícios Python/ex054.py
@@ -1,16 +1,20 @@
 from datetime import date
-soma = 0
-for c in range(1,2):
-    ano = int(input('Em que ano a {}ª pessoa Nasceu? '.format(c)))
 ano_atual = date.today().year
-maior_idade = ano_atual - ano
-if maior_idade >= 21:
-    soma += 1
-    print('Temos {} pessoas maiores de idade.'.format(soma))
+totmaior = 0
+totmenor = 0
+for c in range(1,8):
+    ano = int(input('Em que ano a {}ª pessoa Nasceu? '.format(c)))
+    maior_idade = ano_atual - ano
+    if maior_idade >= 21:
+        totmaior += 1
+    elif maior_idade < 21:
+        totmenor += 1
+print('Ao todo temos {} pessoas menores de idade e {} pessoas maiores de idade.'.format(totmenor, totmaior))
 
-'''elif maior_idade < 21:
-    soma += 1
-    print('Temos {} pessoas menores de idade.'.format(soma))'''
+  
+        
+    
+    
     
 
 
