31ed026ef5e7ea030cbd68bab4128624a79c85d6
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-12-16 10:02:13 -0300

    Update ex068.py

--- Exercícios Python/ex068.py
+++ Exercícios Python/ex068.py
@@ -1,16 +1,34 @@
 from random import randint
 
-soma = 0
-jogador = str(input('Você quer par ou impar? '))
-n = int(input('escolha um número: '))
-computador = randint(0,10)
-soma = n + computador
-print(soma)
-if soma % 2 == 0: #jogou par
-    soma += computador
-    print('Par')
-else:
-    print('Impar')
+print('-=' * 20)
+print('VAMOS JOGAR PAR OU IMPAR!')
+print('-=' * 20)
+
+jog_win = 'GANHOU!'
+jogador = 'PAR' , 'IMPAR'
+while True:
+    if jogador == 'PAR' and jogador == soma % 2 == 0:
+        jogador = str(input('Você quer par ou impar? ')).upper()
+        n = int(input('escolha um número: '))
+        print('-' * 20)
+        computador = randint(0,10)
+        soma = n + computador
+        if soma % 2 == 0:
+            print(f'Você jogou {n} e o computador {computador}. Total de {soma} deu par!')
+        else:
+            print(f'Você jogou {n} e o computador {computador}. Total de {soma} deu impar!')
+        if jogador == 'PAR':
+            if soma % 2 == 0:
+                print(f'Você {jog_win}')
+            elif soma % 1 == 0:
+                print('Você PERDEU')        
+        if jogador == 'IMPAR':
+            if soma % 2 == 0:
+                print('Você PERDEU')
+            elif soma % 1 == 0:
+                print(f'Você {jog_win}')
+
+           
 
         
         
