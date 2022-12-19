c5c820014fd0d8ed05f7a6a9f54412ccf1083ce8
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-12-19 20:15:45 -0300

    Update ex068.py

--- Exercícios Python/ex068.py
+++ Exercícios Python/ex068.py
@@ -4,29 +4,36 @@ print('-=' * 20)
 print('VAMOS JOGAR PAR OU IMPAR!')
 print('-=' * 20)
 
-jog_win = 'GANHOU!'
+cont = 0
 jogador = 'PAR' , 'IMPAR'
-while True:
-    if jogador == 'PAR' and jogador == soma % 2 == 0:
-        jogador = str(input('Você quer par ou impar? ')).upper()
-        n = int(input('escolha um número: '))
-        print('-' * 20)
-        computador = randint(0,10)
-        soma = n + computador
+while cont == cont:
+    jogador = str(input('Você quer par ou impar? ')).upper()
+    n = int(input('escolha um número: '))
+    print('-' * 20)
+    computador = randint(0,10)
+    soma = n + computador
+    if soma % 2 == 0:
+        print(f'Você jogou {n} e o computador {computador}. Total de {soma} deu par!')
+    else:
+        print(f'Você jogou {n} e o computador {computador}. Total de {soma} deu impar!')
+    if jogador == 'PAR':
         if soma % 2 == 0:
-            print(f'Você jogou {n} e o computador {computador}. Total de {soma} deu par!')
-        else:
-            print(f'Você jogou {n} e o computador {computador}. Total de {soma} deu impar!')
-        if jogador == 'PAR':
-            if soma % 2 == 0:
-                print(f'Você {jog_win}')
-            elif soma % 1 == 0:
-                print('Você PERDEU')        
-        if jogador == 'IMPAR':
-            if soma % 2 == 0:
-                print('Você PERDEU')
-            elif soma % 1 == 0:
-                print(f'Você {jog_win}')
+            print('Você GANHOU!')
+            cont += 1
+        elif soma % 1 == 0:
+            print('Você PERDEU')
+            break      
+    if jogador == 'IMPAR':
+        if soma % 2 == 0:
+            print('Você PERDEU')
+            break
+        elif soma % 1 == 0:
+            print('Você GANHOU')
+            cont += 1
+    print('Você venceu. Vamos jogar novamente...')
+print(f'Gamer Over! Você venceu {cont} vezes.')
+
+
 
            
 
