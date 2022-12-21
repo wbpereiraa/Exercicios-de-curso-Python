f2f57c6f96ff47290f53383e3a4a832c32dcdfe7
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-12-21 07:51:28 -0300

    Update ex069.py

--- Exercícios Python/ex069.py
+++ Exercícios Python/ex069.py
@@ -1,7 +1,5 @@
 from collections import Counter
 resposta = 'S'
-i = []
-s = []
 maior_18 = []
 homens = []
 mulher_20 = []
@@ -9,23 +7,21 @@ soma_i = 0
 soma_s = 0
 while resposta == 'S':
     idade = int(input('Digite a idade: '))
-    i.append(idade)
     if idade > 18:
-        maior_18.append(idade)
+        maior_18.append(idade) #função .append adiciona elemento na lista
     if idade < 20:
         mulher_20.append(idade)
     sexo = str(input('Digite o sexo: [M/F]: ')).upper().strip()[0]
-    s.append(sexo)
     if sexo == 'M':
         homens.append(sexo)
     if sexo == 'F':
         mulher_20.append(sexo)
     resposta = str(input('Deseja continuar o cadastro [S/N]? ')).upper().strip()[0]
     
-print(maior_18)
-print(homens)
-print(mulher_20)
-print(f'{(len(maior_18))} pessoas tem mais de 18 anos de idade!')
+#print(maior_18) exibe a lista
+#print(homens) exibe a lista
+#print(mulher_20) exibe a lista
+print(f'{(len(maior_18))} pessoas tem mais de 18 anos de idade!') # função len lê quantos itens tem na lista
 print(f'{(len(homens))} homens foram cadastrados!')
 #usando o método counter() IMPORTANDO A BIBLIOTECA COLLECTIONS (COUNTER)
 x = 'F' #define qual elemento vai ser buscado na lista
