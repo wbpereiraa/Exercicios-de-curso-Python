dc17b5d31655a049168c29546ef4abf0cc531955
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2022-12-22 08:49:44 -0300

    Update ex070.py

--- Exercícios Python/ex070.py
+++ Exercícios Python/ex070.py
@@ -1,14 +1,16 @@
 totG = p1000 = 0
-listaGeral = []
+listaP = []
+listaProd = []
+totalGeral = []
 while True:
     nome_produto = str(input('Nome do produto: '))
-    listaGeral.append(nome_produto) 
+    listaProd.append(nome_produto) 
     preco = float(input('Preço do produto: R$ '))
-    listaGeral.append(preco)  
+    listaP.append(preco)  
     if preco >= 0:
         totG += preco
     if preco > 1000:
-        p1000 += 1   
+        p1000 += 1 
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
@@ -16,6 +18,8 @@ while True:
         break
 print(f'O total gasto da compra é de R$ {totG:.2f}')   
 print(f'{p1000} produtos custam mais de R$1000.00.')
-print(listaGeral)        
-
-        
+while True:
+    tupla = (listaP, listaProd)
+    totalGeral.append(tupla)
+    print(totalGeral)
+    break   
