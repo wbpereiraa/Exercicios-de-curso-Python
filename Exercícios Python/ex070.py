5b836fdb14760faba3ae14b612b0034a435ef552
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2023-01-05 14:58:44 -0300

    ex070 e ex071

--- Exercícios Python/ex070.py
+++ Exercícios Python/ex070.py
@@ -1,4 +1,5 @@
-totG = p1000 = 0
+totG = p1000 = menor = cont = 0
+barato = ' '
 listaP = []
 listaProd = []
 totalGeral = []
@@ -7,10 +8,14 @@ while True:
     listaProd.append(nome_produto) 
     preco = float(input('Preço do produto: R$ '))
     listaP.append(preco)  
+    cont += 1
     if preco >= 0:
         totG += preco
     if preco > 1000:
         p1000 += 1 
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = nome_produto
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
@@ -18,8 +23,4 @@ while True:
         break
 print(f'O total gasto da compra é de R$ {totG:.2f}')   
 print(f'{p1000} produtos custam mais de R$1000.00.')
-while True:
-    tupla = (listaP, listaProd)
-    totalGeral.append(tupla)
-    print(totalGeral)
-    break   
+print(f'O produto mais barato foi {barato} e custa R${menor:.2f}')
---
+++ Exercícios Python/ex071.py
@@ -0,0 +1,23 @@
+total = 0
+valor = int(input('Qual o valor a ser sacado? R$'))
+total = valor
+ced = 50
+totalced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totalced += 1
+    else:
+        if totalced > 0:
+            print(f'Total de {totalced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totalced = 0
+        if total == 0:
+            break
+print('Obrigado volte sempre!')
+
