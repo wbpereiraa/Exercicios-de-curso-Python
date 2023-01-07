3fd36bbb9f2fac9af2dc343cab54d2d79b8f5a08
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2023-01-06 23:56:58 -0300

    Update ex072.py

--- Exercícios Python/ex072.py
+++ Exercícios Python/ex072.py
@@ -4,7 +4,6 @@ n = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 numero = int(input('Digite um número entre 0 e 20: '))
 while True:
     if numero >= 0 and numero <= 20:
-        
         break
     else:
         numero = int(input('Número inválido! Digite um número entre 0 e 20: '))
