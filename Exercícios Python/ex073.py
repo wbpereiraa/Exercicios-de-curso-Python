f2828d198aa17f199b40da972aec318137d27df9
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2023-01-10 19:44:24 -0300

    Create ex073.py

---
+++ Exercícios Python/ex073.py
@@ -0,0 +1,2 @@
+lista_times = ('Palmeiras', 'Internacional', 'Fluminense', 'Corinthians', 'Flamengo', 'Atlético Paranaense', 'Atlético Mineiro', 'Fortaleza', 'São Paulo', 'América', 'Botafogo', 'Santos', 'Goiás', 'Bragantino', 'Coritiba', 'Cuiaba', 'Ceará', 'Goianiense', 'Avaí', 'Juventude')
+
