c287056e831ee3b1467304b756c5e520e16f68c9
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2023-01-25 08:04:25 -0300

    Update ex081.py

--- Exercícios Python/ex081.py
+++ Exercícios Python/ex081.py
@@ -7,4 +7,10 @@ while True:
         resp = str(input('Deseja continuar? [S/N]')).upper().strip()[0]
     if resp == 'N':
         break
+if 5 in lista:
+    print('O número 5 está na lista.')
+else:
+    print('O número 5 não está na lista.')
 print(f'foram digitados {len(lista)} elementos.')
+lista.sort(reverse=True)
+print(f'A ordem da lista em ordem decrescente é {lista}.')
