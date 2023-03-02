1c0bc95321b586ab68644456dc00a456ea583d03
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2023-03-02 07:16:21 -0300

    Create ex097.py

---
+++ Exercícios Python/ex097.py
@@ -0,0 +1,10 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('-' * tam)
+    print(f'  {msg}')
+    print('-' * tam)
+
+#programa principal
+escreva('William Barbosa Pereira')
+escreva('Curso de Python')
+escreva('Oi')
