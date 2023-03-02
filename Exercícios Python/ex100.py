a82aa5e47e6d718d109f6155669caaa96a6ba7d0
Wlliam Barbosa Pereira <wb.pereiraa@gmail.com>
2023-03-02 08:28:23 -0300

    Create ex100.py

---
+++ Exercícios Python/ex100.py
@@ -0,0 +1,24 @@
+from random import randint
+from time import sleep
+
+def sortear(lista):
+    print(f'Sorteando 5 valores da lista: ', end='')
+    for cont in range(0,5):
+        n = randint(1,10)
+        lista.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('PRONTO!')
+
+def somarPar(lista):
+    soma  = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando oa valores pares de {lista}, temos {soma}.')
+
+
+#programa principal
+numeros = list()
+sortear(numeros)
+somarPar(numeros)
